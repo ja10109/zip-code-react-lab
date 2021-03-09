fb89d83e7cffb64091595c66c4d49697c82c608c
Jason Cho <43969888+ja10109@users.noreply.github.com>
2021-03-09 15:21:12 -0500

    Update App.js

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,57 +1,107 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-function City(props) {
-  return (<div>This is the City component</div>);
+function City({data}) {
+  return (
+    <div className="card mb-4">
+      <div className="card-header">
+        { data.City }
+      </div>
+      <div className="card-body">
+        <ul>
+          <li>State: {data.State}</li>
+          <li>Location: ({data.Lat}, {data.Long})</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
+function ZipSearchField({zipCode, onChange}) {
   return (
     <div>
-      <label>Zip Code: </label>
-      <input type="text" onChange=(onChange) />
-    </div>);
+      <form className="form-inline my-4">
+        <label>Zip Code:</label>
+        <input
+          type="text"
+          className="form-control ml-2"
+          value={zipCode}
+          onChange={onChange}
+        />
+      </form>
+    </div>
+  );
 }
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipCode: ' '.
       cities: [],
+      zipCode: '',
     }
   }
-    
-  zipChanged(e) {
-    fetch('https://ctp-zip-api.herokuapp.com/zip/')
-      .then()
-      .then()
-      .catch()
-    
-    
-    this.setState({
-      zipCode: e.target.value
-    })
-  }
-          
+  zipChanged(event) {
+    let zip = event.target.value;
+    console.log(zip);
+
+    if(zip.length === 5) {
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then((res) => res.json())
+      .then((body) => {
+        console.log(body);
+        this.setState({
+          cities: body
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+        this.setState({
+          cities: []
+        })
+      })
+    } else {  
+      this.setState({
+        cities: []
+      })
+  } 
+  this.setState({
+    zipCode: event.target.value
+  })
+}
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField onChange={(e) => this.zipChange(e)} />
-        <div>
-          <City />
-          <City />
-          <City />
-          <City />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <ZipSearchField
+                zipCode={this.state.zipCode}
+                onChange={(e) => this.zipChanged(e)}
+              />
+            </div>
+          </div>
+          {
+            this.state.cities.length === 0
+            ? <h1>No Results</h1> 
+            : this.state.cities.map((c, index) => {
+                return (
+                  <div className="row" key={index}>
+                    <div className="col">
+                      <City
+                        data={c}
+                      />
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
-
 export default App;
